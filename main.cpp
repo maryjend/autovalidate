f5d3ffe63686398b721986c4a5c22e7e46dc4ee1
Mary Jendricks <mejendricks@csuchico.edu>
2025-09-24 11:16:41 -0700

    Function will terminate if nothing is entered. Closes #1

--- main.cpp
+++ main.cpp
@@ -24,9 +24,12 @@ int main(){
   do{
     cout << "What's next?\n";
     getline(cin,input);
+    if (input == "nothing") { 
+      break;
+    } 
     pick = rand() % 4;
     cout << VALIDATION[pick] << "!\n";
-  }while( input != "nothing" );
+  }while(true);
 
   return 0;
 }
